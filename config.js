ed2d76a2811f99c1cd0bb6229b4547c51b59eb26
yeomchangjeong76-ai <yeomchangjeong76@gmail.com>
2026-01-16 20:42:03 +0900

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
   TOKEN: "",
   language: "en",
   ownerID: ["1004206704994566164", ""], 
-  mongodbUri : "mongodb+srv://shiva:shiva@musicbotyt.ouljywv.mongodb.net/?retryWrites=true&w=majority",
+  mongodbUri : "mongodb+srv://ECAFOUNDATION:playtime08/@tdiscordbot.1uigtva.mongodb.net/?appName=TDiscordBot",
   spotifyClientId : "d92baed9605a45a39ed7c2a2d960b1c1",
   spotifyClientSecret : "e9b29f6739de4315bc03b6d8a8e93b03",
   setupFilePath: './commands/setup.json',
